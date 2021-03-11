ba87d3113efc790d65bd0a68d5496939c7535bb9
andronicaa <alexandraandronic368@gmail.com>
2021-03-11 14:32:34 +0200

    Modificari pagina principala

--- appleorchard/src/MainPage.js
+++ appleorchard/src/MainPage.js
@@ -3,7 +3,7 @@ import { Card, Button, Alert } from 'react-bootstrap';
 import { useAuth } from './context/AuthContext';
 import { Link, useHistory } from 'react-router-dom';
 import styles from "./Styles/MainPage.module.css";
-import TempProfile from "./Components/TempProfile";
+import Profile from "./Components/TempProfile";
 export default function MainPage() {
     const[error, setError] = useState('');
     const{ currentUser, logout } = useAuth();
@@ -23,22 +23,9 @@ export default function MainPage() {
 
     return (
     <>
-      <Card className={styles.cardProfile}>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-          <div className="w-100 text-center mt-2">
-            <Button variant="link" onClick={handleLogout}>
-              <strong>Log Out</strong>
-            </Button>
-          </div>
-          <TempProfile />
-        </Card.Body>
-      </Card>
+     <div className={styles.profileCard}>
+        <Profile />
+     </div>
     </>
     )
 }
