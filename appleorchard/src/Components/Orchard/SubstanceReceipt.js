c9e1feed985afee60894e6500c0d2b99def2d335
andronicaa <alexandraandronic368@gmail.com>
2021-03-31 14:43:14 +0300

    Tabel facturi substante cu paginare

--- appleorchard/src/Components/Orchard/SubstanceReceipt.js
+++ appleorchard/src/Components/Orchard/SubstanceReceipt.js
@@ -6,6 +6,7 @@ import { useAuth } from '../../Firebase/context/AuthContext';
 import Modal from 'react-bootstrap/Modal';
 import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
+import { useTable, usePagination } from 'react-table';
 
 
 export default function SubstanceReceipt() {
@@ -16,7 +17,7 @@ export default function SubstanceReceipt() {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    const [receipts, setReceipts] = useState([]);
+    const [data, setReceipts] = useState([]);
     const measureQuantity = useRef();
     const currency = useRef();
     const product = useRef();
@@ -60,6 +61,52 @@ export default function SubstanceReceipt() {
         
     }
 
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Nume',
+                accessor: 'product'
+                
+            },
+            {
+                Header: 'Pret',
+                accessor: 'price'
+            },
+            {
+                Header: 'Cantitate',
+                accessor: 'quantity'
+            },
+            {
+                Header: 'Luna achizitie',
+                accessor: 'month'
+            }
+        ],
+        []
+    );
+
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: {pageIndex, pageSize},
+    } = useTable(
+        {
+            columns, data, initialState: {pageIndex: 0},
+        },
+        usePagination
+    )
+    
     function listReceipt() {
         refCurrentUser.onSnapshot((querySnapshot) => {
           const items = [];
@@ -83,8 +130,8 @@ export default function SubstanceReceipt() {
 
     function totalPriceOfSubstances() {
         var totalPriceLocal = 0;
-        console.log(receipts);
-        receipts.map((prd) => {
+        console.log(data);
+        data.map((prd) => {
             totalPriceLocal += prd.price; 
         });
         console.log("Pretul total local", totalPriceLocal);
@@ -98,37 +145,80 @@ export default function SubstanceReceipt() {
         listReceipt();
         totalPriceOfSubstances();
     }, []);
+
+
     return (
-        <div>
-            <Card className={styles.receiptCard}>
-                <Card.Body>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Produs</th>
-                                <th>Pret</th>
-                                <th>Cantitate</th>
-                                <th>Luna achizitie</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                receipts.map((rec) => (
-                                <tr key={rec}>
-                                    <td>{rec.product}</td>
-                                    <td>{rec.price}</td>
-                                    <td>{rec.quantity}</td>
-                                    <td>{rec.month}</td>
-                                </tr>
-                                ))
-                            }
-                        </tbody>
-                        <div>
-                            <p><strong>Pretul total al facturilor:</strong> {totalPrice}</p>
-                        </div>
-                    </Table>
-            
-                <button className="btn btn-success" onClick={handleShow}><i class="fa fa-plus" aria-hidden="true"></i>&nbsp;Adauga factura</button>
+    <>
+        <Card className={styles.receiptCard}>
+            <Card.Body>
+                <Table {...getTableProps()}>
+                    <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                        </tr>
+                    ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                    {page.map((row, i) => {
+                        prepareRow(row)
+                        return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                        )
+                    })}
+                    </tbody>
+                </Table>
+                <div className="pagination">
+                    <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                        {'<<'}
+                    </button>{' '}
+                    <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                        {'<'}
+                    </button>{' '}
+                    <button onClick={() => nextPage()} disabled={!canNextPage}>
+                        {'>'}
+                    </button>{' '}
+                    <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                        {'>>'}
+                    </button>{' '}
+                        <span>
+                        Page{' '}
+                        <strong>
+                            {pageIndex + 1} of {pageOptions.length}
+                        </strong>{' '}
+                        </span>
+                        <span>
+                        | Go to page:{' '}
+                        <input
+                            type="number"
+                            defaultValue={pageIndex + 1}
+                            onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                            }}
+                            style={{ width: '100px' }}
+                        />
+                        </span>{' '}
+                        <select
+                        value={pageSize}
+                        onChange={e => {
+                            setPageSize(Number(e.target.value))
+                        }}
+                        >
+                        {[1, 2, 3].map(pageSize => (
+                            <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                            </option>
+                        ))}
+                        </select>
+                </div>
+            <button className={`btn btn-success ${styles.addReceiptButton}`} onClick={handleShow}><i class="fa fa-plus" aria-hidden="true"></i>&nbsp;Adauga factura</button>
                 <Modal show={show} onHide={handleClose}>
                     <Modal.Body>
                         <div>
@@ -213,8 +303,8 @@ export default function SubstanceReceipt() {
                         </div>
                     </Modal.Body>
                 </Modal>
-                </Card.Body>
-                </Card>
-        </div>
+            </Card.Body>
+        </Card>
+    </>
     )
 }
