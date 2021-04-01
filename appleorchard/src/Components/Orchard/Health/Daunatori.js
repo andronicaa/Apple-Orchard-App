73764376eae432e236a0347eb67bf82d399c5d2e
andronicaa <alexandraandronic368@gmail.com>
2021-04-01 16:39:16 +0300

    Adaugare navbar + schelet pagina daunatori

--- appleorchard/src/Components/Orchard/Health/Daunatori.js
+++ appleorchard/src/Components/Orchard/Health/Daunatori.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import daunatori from "../../../Imgs/daunatori_1.jpg";
 import styles from "./Style/Daunatori.module.css";
+import OrchardMenu from '../OrchardMenu';
 
 
 
 
 export default function Daunatori() {
     return (
+        <>
+        <OrchardMenu />
         <div className={styles.mainCard}>
             <Card>
                 <Card.Img variant="top" src={daunatori} className={styles.imgCard}/>
@@ -16,5 +19,6 @@ export default function Daunatori() {
                 </Card.Body>
             </Card>
         </div>
+        </>
     )
 }
