8ca296f18adbcabd2b3c979916d56bba8f1c6234
andronicaa <alexandraandronic368@gmail.com>
2021-06-26 13:33:07 +0300

    Curatare cod

--- appleorchard/src/Components/AuthHome/MainPage.js
+++ appleorchard/src/Components/AuthHome/MainPage.js
@@ -1,28 +1,9 @@
-import React, { useState } from 'react';
-import { useAuth } from '../../Firebase/context/AuthContext';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import styles from "./Styles/MainPage.module.css";
-import Profile from "../Feed/TempProfile";
 import Header from "../Header/Header";
 
 
 export default function MainPage() {
-    const[error, setError] = useState('');
-    const{ currentUser, logout } = useAuth();
-    const history = useHistory();
-
-    async function handleLogout() {
-        setError("");
-
-        try {
-            await logout();
-            history.push("/neauth-home");
-        } catch {
-            setError("Failed to log out");
-        }
-    }
-
-
     return (
     <div className={styles.mainPage}>
         <Header />
